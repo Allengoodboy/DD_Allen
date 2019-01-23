89e03524140be8bcb7d38d1eab51d3055b4f6b76
DESKTOP-BJF1CLL\DD <jnhguaishou@163.com>
2019-01-23 19:41:49 +0800

    1/23

---
+++ zhundian_project/src/utils/data.js
@@ -0,0 +1,152 @@
+function getNavList() {
+    var arr;
+    arr=[{
+        id: 0,
+        'iconPath': '../images/icon/sync.png',
+        'text': '全部订单'
+    },{
+        id: 1,
+        'iconPath': '../images/icon/menu.png',
+        'text': '待付款'
+    },{
+        id: 2,
+        'iconPath': '../images/icon/font-colors.png',
+        'text': '进行中'
+    },{
+        id: 3,
+        'iconPath': '../images/icon/check.png',
+        'text': '待评价'
+    }]
+    return arr;
+}
+function getNavgation() {
+    var arr;
+    arr=[{
+        'iconPath': '../images/icon/pushpin-fill.png',
+        'text': '我的地址管理'
+    },{
+        'iconPath': '../images/icon/star.png',
+        'text': '招募城市合伙人'
+    },{
+        'iconPath': '../images/icon/phone-fill.png',
+        'text': '在线客服'
+    },{
+        'iconPath': '../images/icon/ellipsis.png',
+        'text': '更多'
+    }]
+    return arr;
+}
+function getadList() {
+    var arr;
+    arr=[
+        {'image_Path':'../images/pic/test1.jpg'},
+        {'image_Path': '../images/pic/test2.jpg'}
+        ]
+        return arr;
+}
+function getnavList() {
+    var arr;
+    arr=[
+        {id:1,'text':'保洁清洗','image_Path':'../images/icon/check.png'},
+        {id:2,'text':'防水补漏','image_Path':'../images/icon/icon_home.png'},
+        {id:3,'text':'管理疏通','image_Path':'../images/icon/icon_home_active.png'},
+        {id:4,'text':'家电修装','image_Path':'../images/icon/font-colors.png'},
+        {id:5,'text':'家具装修','image_Path':'../images/icon/menu.png'},
+        {id:6,'text':'房屋翻新','image_Path':'../images/icon/check.png'},
+        {id:7,'text':'瓷妆美缝','image_Path':'../images/icon/icon_home.png'},
+        {id:8,'text':'艺术斜料','image_Path':'../images/icon/icon_home_active.png'},
+        {id:9,'text':'智能门锁','image_Path':'../images/icon/font-colors.png'},
+        {id:10,'text':'更多','image_Path':'../images/icon/ellipsis.png'},
+    ]
+    return arr;
+}
+function getdetailList() {
+    var arr;
+    arr=[
+        {'icon_Path':'../images/pic/test1.jpg','name':'钟点工保洁','content':'家庭日常保洁'},
+        {'icon_Path':'../images/pic/test1.jpg','name':'空调维修','content':'专业维修师傅，高效便捷'},
+        {'icon_Path':'../images/pic/test1.jpg','name':'洗衣机清洗','content':'洗衣机清洗又快又干净'},
+        {'icon_Path':'../images/pic/test1.jpg','name':'冰箱清洗','content':'冰箱清洗'},
+        {'icon_Path':'../images/pic/test1.jpg','name':'热水器清洗','content':'热水器内胆清洗'},
+    ]
+    return arr;
+}
+function getaddress() {
+    var arr;
+    arr= [
+        {id:1,'name':'张三','tel':'13616988547','address':'华石路154号6楼'},
+        {id:2,'name':'爸b','tel':'13616125554','address':'华石路1558号12楼'},
+        {id:3,'name':'吖肥','tel':'13616987932','address':'华石路12131号121321楼'},
+    ]
+    return arr;
+}
+function getcurrentTab_list() {
+    var arr;
+    arr= [{
+        id: 0,
+        dataset: [
+            {'style':'全款','zt':'进行中','name':'保洁清洗0','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保洁清洗1','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'全款','zt':'进行中','name':'保洁清洗2','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保洁清洗3','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'全款','zt':'进行中','name':'保洁清洗4','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保洁清洗5','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'全款','zt':'进行中','name':'保洁清洗6','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保洁清洗7','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'全款','zt':'进行中','name':'保洁清洗0','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保洁清洗1','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'全款','zt':'进行中','name':'保洁清洗2','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保洁清洗3','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'全款','zt':'进行中','name':'保洁清洗4','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保洁清洗5','address':'中山市石岐区大新南路1号','time':20181220},
+        ]
+    },{
+        id: 1,
+        dataset: [
+            {'style':'全款','zt':'进行中','name':'抽油烟机清洗1','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'全款','zt':'进行中','name':'油烟机清洗2','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'油烟机清洗3','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'油烟机清洗4','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'全款','zt':'进行中','name':'油烟机清洗5','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'油烟机清洗6','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保油烟机清洗7','address':'中山市石岐区大新南路1号','time':20181220},
+        ]
+    },{
+        id: 2,
+        dataset: [
+            {'style':'定金','zt':'待付款','name':'清洗完毕2','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保洁清洗','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保洁清洗','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待付款','name':'保洁清洗','address':'中山市石岐区大新南路1号','time':20181220},
+        ]
+    },{
+        id: 3,
+        dataset: [
+            {'style':'定金','zt':'待评价','name':'有排清3','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待评价','name':'保洁清洗','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待评价','name':'保洁清洗','address':'中山市石岐区大新南路1号','time':20181220},
+            {'style':'定金','zt':'待评价','name':'保洁清洗','address':'中山市石岐区大新南路1号','time':20181220},
+        ]
+    }
+    ]
+    return arr;
+}
+function getCoupon() {
+    var arr;
+    arr=[
+        {id:1,'name':'省5元：代金券','choice':false},
+        {id:2,'name':'省3元：代金券','choice':false},
+        {id:3,'name':'不使用优惠','choice':false},
+    ]
+    return arr;
+}
+module.exports={
+    getNavList : getNavList,
+    getNavgation : getNavgation,
+    getadList : getadList,
+    getnavList : getnavList,
+    getdetailList: getdetailList,
+    getaddress: getaddress,
+    getcurrentTab_list: getcurrentTab_list,
+    getCoupon: getCoupon
+}
